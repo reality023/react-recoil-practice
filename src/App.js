e9f062fda0da7fdab79afc878f06d39086651261
Gridy <reality023@naver.com>
2022-07-05 18:05:33 +0900

    최종 코드

--- src/App.js
+++ src/App.js
@@ -1,58 +1,29 @@
-import { useRef } from "react";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { useEffect, useRef, useState } from "react";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
-import { myList, myListbyCategory, selectedCategory } from "./atoms";
+import { minutes, hours } from "./atoms";
 
 function App() {
-  const inputRef = useRef(null);
-  const selectRef = useRef(null);
-  // const [list, setList] = useRecoilState(myList);
-  const [list, setList] = useRecoilState(myListbyCategory);
-  const [selected, setSelected] = useRecoilState(selectedCategory);
-
-  const onClick = (e) => {
-    const content = inputRef.current.value;
-    // setList((current) => { // 불변성
-    //   return [...current, { id: Date.now(), content }];
-    // });
-    setList({ id: 1, content, category: selected });
-  }
-
-  const remove = (id) => {
-    setList((current) => {
-      return current.filter((v) => v.id !== id);
-    });
-  }
+  const [minute, setMinute] = useRecoilState(minutes);
+  const [hour, setHour] = useRecoilState(hours);
+  // const [minute, setMinute] = useState(0);
 
   const onChange = (e) => {
-    setSelected(selectRef.current.value);
+    setMinute(e.target.value);
+  }
+
+  const onChangeHour = (e) => {
+    setHour(e.target.value);
   }
 
   return (
     <Container>
-      <select ref={selectRef} onChange={onChange}>
-        <option value="cate-1">카테고리1</option>
-        <option value="cate-2">카테고리2</option>
-      </select>
-      {
-        list.map((value, idx) => {
-          return (
-            <List key={idx}>
-              <div>{value.content}</div>
-              <button onClick={(event) => { remove(value.id) }}>삭제</button>
-            </List>
-          )
-        })
-      }
-      <input type="text" ref={inputRef} />
-      <button onClick={onClick}>추가</button>
+      <input type="number" value={minute} onChange={onChange} />
+      <input type="number" value={hour} onChange={onChangeHour} />
     </Container>
   );
 }
 
 const Container = styled.div``;
-const List = styled.div`
-  border: 1px solid #ddd;
-`;
 
 export default App;
--- src/atoms.js
+++ src/atoms.js
@@ -1,51 +1,18 @@
 import { atom, selector } from "recoil";
 
-export const selectedCategory = atom({
-  key: "category",
-  default: "cate-1"
+export const minutes = atom({
+  key: "minutes",
+  default: 0
 });
 
-export const myList = atom({
-  key: "list",
-  default: [
-    {
-      id: 1,
-      category: "cate-1",
-      content: "내용1"
-    },
-    {
-      id: 2,
-      category: "cate-1",
-      content: "내용2"
-    },
-    {
-      id: 3,
-      category: "cate-2",
-      content: "내용3"
-    },
-    {
-      id: 4,
-      category: "cate-2",
-      content: "내용4"
-    },
-    {
-      id: 5,
-      category: "cate-1",
-      content: "내용5"
-    }
-  ],
-});
 
-export const myListbyCategory = selector({
-  key: "myListbyCategory",
+export const hours = selector({
+  key: "hours",
   get: ({ get }) => {
-    const list = get(myList);
-    const cate = get(selectedCategory);
-    return list.filter((item) => item.category === cate);
+    const minute = get(minutes);
+    return minute / 60;
   },
-  set: ({ get, set }, newValue) => {
-    // newValue = { id: 1, content: "내용" }
-    const list = get(myList);
-    set(myList, [...list, newValue]);
+  set: ({ set }, hour) => {
+    set(minutes, hour * 60);
   }
 });
